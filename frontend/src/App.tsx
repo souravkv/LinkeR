8d1934dae11629e5fc2d54cfb9525b04c8028eaf
Sourav KV <spexod@gmail.com>
2024-05-19 05:01:35 +0530

    new design larger bg

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
 
 
         <div className='  z-40    text-white right-0 top-0 p-5 fixed'><CiMenuBurger size={30} /></div>
-        <div className='  bg-cover bg-right  md:bg-left bg-black   bg-[url("https://images.unsplash.com/photo-1500408698778-2afa347782f3?q=80&w=2992&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")]  h-[200vh] w-screen  '>
+        <div className='  bg-cover bg- bg-right  md:bg-left bg-black   bg-[url("https://images.unsplash.com/photo-1500408698778-2afa347782f3?q=80&w=2992&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")] h-[410vh]  md:h-[300vh] w-screen  '>
 
           <div className=' h-screen'>
             <Hero />
