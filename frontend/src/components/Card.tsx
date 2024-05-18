2ce4d1397d319cc96fce241593c7c7f76f4156c3
Sourav KV <spexod@gmail.com>
2024-05-18 15:55:25 +0530

    added a link

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -3,8 +3,8 @@
 function Card() {
     return (
         <div onClick={() => {
-            window.open('https://www.youtube.com/@t3dotgg', "_blank")
-        }} className='     hover:brightness-50  ease-in duration-200   bg-cover  bg-opacity-50  h-[20vh]  bg-[url("https://images8.alphacoders.com/132/1325051.jpeg")]   shadow-xl m-3 rounded-xl border border-gray-100 flex justify-center text-center ' >
+            window.open('https://www.youtube.com/watch?v=D28YLKppg5U&feature=youtu.be', "_blank")
+        }} className='     hover:brightness-50  ease-in duration-200   bg-cover   bg-opacity-50  h-[20vh]  bg-[url("https://images8.alphacoders.com/132/1325051.jpeg")]   shadow-xl m-3 rounded-xl border border-gray-100 flex justify-center text-center ' >
 
             card 1
 
