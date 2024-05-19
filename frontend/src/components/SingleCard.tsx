0ad4ee7d4419bf867cb3a9eba7fe3320d28d4837
Sourav KV <spexod@gmail.com>
2024-05-19 17:08:08 +0530

    delet icon

--- frontend/src/components/SingleCard.tsx
+++ frontend/src/components/SingleCard.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { MdOutlineDeleteOutline } from "react-icons/md";
 import { useState } from "react"; // Import useState instead of useEffect for managing state
 
 function SingleCard({ title, link, thumbnail, watched }: { title: string, link: string, thumbnail: string, watched: boolean }) {
@@ -30,13 +31,13 @@ function SingleCard({ title, link, thumbnail, watched }: { title: string, link: 
     }
 
     return (
-        <div className="group md:ease-out duration-500 hover:-translate-y-12 h-[15vh]">
+        <div className="group md:ease-out duration-500 hover:-translate-y-12 h-[15vh] md:h-[20vh]">
             <div className="relative brightness-75 font-thin text-xl text-white md:hover:text-2xl hover:brightness-100 bg-cover bg-opacity-50 h-[13vh] md:h-[15vh] shadow-xl m-3 rounded-xl flex justify-start items-end text-center" style={{ backgroundImage: `url(${thumbnail})` }}>
                 <div onClick={() => { window.open(link, "_blank"); }} className="text-xs translate-y-[20px] absolute bottom-0 right-0 text-shadow-lg brightness-100 w-full text-left">{title}</div>
                 {!watched && <div onClick={() => { watcher(link, true) }} style={{}} className="cursor-pointer absolute top-0 right-0 px-1 translate-x-2 mt-2 text-sm text-green-400 rounded-full bg-green-600">watched ?</div>}
             </div>
             <div className="w-full flex justify-end px-4">
-                <div onClick={() => { remover(link) }} className="hidden group-hover:block hover:bg-red-700 bg-red-900 rounded-lg text-[10px] inline-block p-1"> remove</div>
+                <div onClick={() => { remover(link) }} className="hidden group-hover:block hover:bg-red-700 bg-red-900 rounded-lg text-[10px] inline-block p-1"> <MdOutlineDeleteOutline size={15} color="gray" /></div>
             </div>
         </div>
     );
